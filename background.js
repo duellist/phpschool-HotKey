59ee70637cd91a6e31c047eff6eab4dfad2249e2
duellist <duellist@naver.com>
2015-10-01 15:32:13 +0900

    Revert "phpschool HotKey"
    This reverts commit 26e3477a1ae2b836fd31854135c393d986a3c40e.

--- background.js
+++
@@ -1,6 +0,0 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
-    if (tab.url.indexOf('www.phpschool.com') > -1) {
-        chrome.pageAction.show(tabId);
-    }
-};
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
